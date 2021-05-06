ebccd68fc49d4c47da4e72da9a03e204037f2b6d
asmita-shrivastav <47494142+asmita-shrivastav@users.noreply.github.com>
2021-05-05 22:18:02 -0700

    Add files via upload

---
+++ deadlockSolution.java
@@ -0,0 +1,46 @@
+package assignment;
+
+public class deadlockSolution {
+	   public static Object Lock1 = new Object();
+	   public static Object Lock2 = new Object();
+	   
+	   public static void main(String args[]) {
+	      ThreadDemo1 T1 = new ThreadDemo1();
+	      ThreadDemo2 T2 = new ThreadDemo2();
+	      T1.start();
+	      T2.start();
+	   }
+	   
+	   private static class ThreadDemo1 extends Thread {
+	      public void run() {
+	         synchronized (Lock1) {
+	            System.out.println("Thread 1 has lock 1");
+	            
+	            try {
+	               Thread.sleep(10);
+	            } catch (InterruptedException e) {}
+	            System.out.println("Thread 1 waits for lock 2");
+	            
+	            synchronized (Lock2) {
+	               System.out.println("Thread 1 has lock 1 and 2");
+	            }
+	         }
+	      }
+	   }
+	   private static class ThreadDemo2 extends Thread {
+	      public void run() {
+	         synchronized (Lock1) {
+	            System.out.println("Thread 2 has lock 1");
+	           
+	            try {
+	               Thread.sleep(10);
+	            } catch (InterruptedException e) {}
+	            System.out.println("Thread 2 waits for lock 2");
+	            
+	            synchronized (Lock2) {
+	               System.out.println("Thread 2 has lock 1 and 2");
+	            }
+	         }
+	      }
+	   } 
+	}
